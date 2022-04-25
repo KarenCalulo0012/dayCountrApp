f07641f75db8e7341db38587cfc5cf97430637b2
Karen Calulo <kcalulo@gmail.com>
2022-04-25 18:20:40 +0800

    Add Login Screen UI

--- app/src/main/java/com/appscals/daycountr/MainActivity.kt
+++ app/src/main/java/com/appscals/daycountr/MainActivity.kt
@@ -3,16 +3,11 @@ package com.appscals.daycountr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.appscals.daycountr.presentation.components.LoginScreen
 import com.appscals.daycountr.ui.theme.DayCountrTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,29 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Hello $name!")
-        Spacer(modifier = Modifier.padding(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "BUTTON")
-        }
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DayCountrTheme {
-        Greeting("Android")
     }
 }

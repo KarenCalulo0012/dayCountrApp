86395f9348da797abb522a05bc7e15cfb0371d5c
Karen Calulo <kcalulo@gmail.com>
2022-04-25 22:53:03 +0800

    Add Events Screen

---
+++ app/src/main/java/com/appscals/daycountr/data/EventsMockData.kt
@@ -0,0 +1,40 @@
+package com.appscals.daycountr.data
+
+import com.appscals.daycountr.model.Events
+
+val eventList = listOf(
+    Events(
+        1,
+        "EJ BDAY",
+        " June 7, 2022",
+    ),
+    Events(
+        2,
+        "Election Day",
+        "May 9, 2022",
+    ),
+    Events(
+        2,
+        "Kevin's BDAY",
+        "May 25, 2022",
+    ),
+    Events(
+        2,
+        "Jinx BDAY",
+        "June 17, 2022",
+    ), Events(
+        2,
+        "Ina BDAY",
+        "June 18, 2022",
+    ),
+    Events(
+        2,
+        "Karen BDAY",
+        "March 12, 2022",
+    ),
+    Events(
+        2,
+        "Meralco Payment Due",
+        "April 30, 2022",
+    )
+)
--- app/src/main/java/com/appscals/daycountr/presentation/navigation/FragmentNav.kt
+++ app/src/main/java/com/appscals/daycountr/presentation/navigation/FragmentNav.kt
@@ -1,5 +1,6 @@
 package com.appscals.daycountr.presentation.navigation
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,12 +10,13 @@ import com.appscals.daycountr.presentation.components.SettingsScreen
 
 @Composable
 fun FragmentNav(fragmentNavController: NavHostController) {
+    val scrollState = rememberScrollState()
     NavHost(
         navController = fragmentNavController,
         startDestination = BottomNavData.Event.route.toString()
     ) {
         BottomNavData.Event.route?.let {
-            composable(it) { EventScreen() }
+            composable(it) { EventScreen(scrollState) }
         }
         BottomNavData.Settings.route?.let {
             composable(it) { SettingsScreen() }
